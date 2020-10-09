28f26298aa17cabd9961bc3ef3c54f96b84362b6
Alfonso <a.catanzaro@systemretail.it>
2020-10-09 09:24:01 +0200

    other tests

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import { app, ipcMain, BrowserWindow } from 'electron';
 import path from 'path';
 
-
 declare const MAIN_WINDOW_WEBPACK_ENTRY: any;
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
   app.quit();
 }
+
 
 const createWindow = (): void => {
   // Create the browser window.
@@ -19,11 +19,18 @@ const createWindow = (): void => {
     }
   });
 
+  try {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    require('electron-reload')(__dirname);
+  // eslint-disable-next-line no-empty
+  } catch (_) { }
+
+
   // and load the index.html of the app.
   mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
@@ -51,6 +58,7 @@ app.on('activate', () => {
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
 ipcMain.handle('open-the-window', (event, ...arg) => {
+  console.log(arg);
   const win = new BrowserWindow({
     height: 200,
     width: 200, webPreferences: {
@@ -59,8 +67,12 @@ ipcMain.handle('open-the-window', (event, ...arg) => {
   });
 
   // and load the index.html of the app.
-  const p = path.join("file:////", __dirname, "modal.html");
-  console.log(p);
+
+  const p = path.join("file:////", __dirname, "../renderer/modal/index.html");
   win.loadURL(p);
   win.show();
+})
+
+ipcMain.handle('launch-command', (event, ...arg) => {
+  console.log(arg);
 })
---
+++ src/modal.ts
@@ -0,0 +1,42 @@
+/**
+ * This file will automatically be loaded by webpack and run in the "renderer" context.
+ * To learn more about the differences between the "main" and the "renderer" context in
+ * Electron, visit:
+ *
+ * https://electronjs.org/docs/tutorial/application-architecture#main-and-renderer-processes
+ *
+ * By default, Node.js integration in this file is disabled. When enabling Node.js integration
+ * in a renderer process, please be aware of potential security implications. You can read
+ * more about security risks here:
+ *
+ * https://electronjs.org/docs/tutorial/security
+ *
+ * To enable Node.js integration in this file, open up `main.js` and enable the `nodeIntegration`
+ * flag:
+ *
+ * ```
+ *  // Create the browser window.
+ *  mainWindow = new BrowserWindow({
+ *    width: 800,
+ *    height: 600,
+ *    webPreferences: {
+ *      nodeIntegration: true
+ *    }
+ *  });
+ * ```
+ */
+
+import './index.css';
+import { ipcRenderer } from 'electron'
+
+console.log("Ciao!");
+
+console.log(document.getElementById("modal-button-1"));
+
+document.getElementById("modal-button-1").addEventListener("click", () => {
+  console.log("missile1");
+  ipcRenderer.invoke('launch-command',"missile1");
+})
+document.getElementById("modal-button-2").addEventListener("click", () => {
+  ipcRenderer.invoke('launch-command', ["missile2"]);
+})
